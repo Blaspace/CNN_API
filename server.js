a8eed33ab0d800633ff7a6eb08dfdb90751c09b8
James Ngbede Samuel <115324109+Blaspace@users.noreply.github.com>
2023-11-06 14:01:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const PORT = 8000
+const cors = require('cors')
 const express = require('express')
 const router = require('./routes/AllRoutes')
 
 const app = express()
-
+app.use(cors())
 app.use(router)
 
 app.listen(PORT, ()=> console.log(`app running on ${PORT}`))
