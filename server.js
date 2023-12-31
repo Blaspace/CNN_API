53ff7a377d6ce8396de0496633b8634bafc9ad5b
Blaspace <115324109+Blaspace@users.noreply.github.com>
2023-12-31 13:23:52 -0800

    fgy

--- server.js
+++ server.js
@@ -5,7 +5,5 @@ const router = require("./routes/AllRoutes");
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 app.use(router);
-
 app.listen(3500, () => console.log(`app running on ${3500}`));
